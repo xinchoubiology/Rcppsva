73f02237b0bbe85cbeeb55f395fe6bd696d3d2de
xinchoubiology <xinchoubiology@gmail.com>
2015-06-02 13:18:53 -0400

    bump hunting Engine

--- R/bumphunter.R
+++ R/bumphunter.R
@@ -9,3 +9,37 @@ setMethod("print", signature(x = "bumps"),
               cat(sprintf("\t%s = %s \n", params, x@algorithm[[params]]))
             }
           })
+
+
+##' bumphuntingEigen
+##' 
+##' @title bumphuntingEigen
+##' @description bump hunting algorithm for 450k methylation microarray
+##' @param dat.m 450k methylation microarray matrix via preprocessing; m x n matrix
+##' @param design design matrix; [covariate of interested, paired info, surrogate variables]
+##' @param chr chromosome vector for probes
+##' @param pos position vector for probes
+##' @param cluster The clusters of locations that are to be analyzed together. via correlation
+##'        perspevtive, we can use agglomerate cluster index to bump analysis. If cluster is not
+##'        available, clusterMaker can be used to cluster nearby locations
+##' @param coef Integer. covariate of interest's column
+##' @param cutoff numeric value. Value of estimates coefficient of covariate of interested above cutoff
+##'        or below the negative of cutoff will be used as candidate bump regions. 
+##' @param pvalue numeric value. cutoff of pvalue for candidate regions selection
+##' @param maxGap if cluster is not availabel. maxGap is used by clusterMaker to define cluster
+##' @param minDist if clusters are build, the mininal distance between clusters is setted as constraint
+##' @param nullMethod Method for generating null candidate regions. If ncol(design) > 2. bootstrap method is recommanded
+##' @param smooth logic. If TRUE then the standard error or correlation of point-wise estimatrs will be used as weigths 
+##'        in the \code{loessByCluster}
+##' @param smoother smooth function to estimate genome profile.
+##' @param B integer; Denoting the number of resamples to computr null distribution. Default = 0
+##' @param permutation matrix; define matrix to describe permutations to generate null distribution
+##' @param verbose logical. Optional printing progress message or not
+##' @export
+bumphuntingEigen <- function(dat.m = NULL, design, chr, pos, cluster = NULL, coef = 2,
+                             cutoff = NULL, pvalue = 0.01, maxGap = 500, minDist = 500,
+                             nullMethod = c("permutation", "bootstrap"),
+                             smooth = FALSE, smoother = bumphunter::loessByCluster,
+                             B = 10000, permutation = NULL, verbose = TRUE){
+  
+}
--- R/wilcox.test.R
+++ R/wilcox.test.R
@@ -19,26 +19,36 @@
 wilcox.fit <- function(dat.m = NULL, alternative = c("two.sided", "greater", "less"), qvalue0 = 0.1, mcore = 4){
   alternative <- match.arg(alternative)
   registerDoMC(cores = mcore)
-  rank.m <- aaply(dat.m, 1, .fun = function(d){
-                    r <- rep(0, length(d))
-                    r[which(d != 0)] <- rank(d[which(d != 0)])
-                  },
-                  .parallel = TRUE)
-  sgn.m  <- aaply(dat.m, 1, sign, .parallel = TRUE)
-  W      <- abs(rowSums(rank.m * sgn.m))
-  Nr     <- rowSums(abs(sgn.m))
-  sigma  <- sqrt(Nr * (Nr + 1) * (2 * Nr + 1)/6)
-  p <- pnorm(W, mean = 0.5, sd = sigma, lower.tail = TRUE)
-  pval <- switch(alternative,
-                  two.sided = pmin(p, 1-p),
-                  greater   = 1-p,
-                  less      = p
-                )
-  qval <- p.adjust(pval, method = "fdr")
-  sig.cg <- which(qval <= qvalue0)
+  if(ncol(dat.m) >= 10){
+    rank.m <- aaply(dat.m, 1, .fun = function(d){
+                                      r <- rep(0, length(d))
+                                      r[which(d != 0)] <- rank(d[which(d != 0)])
+                                     }, .parallel = TRUE
+                    )
+    sgn.m  <- aaply(dat.m, 1, sign, .parallel = TRUE)
+    W      <- abs(rowSums(rank.m * sgn.m))
+    Nr     <- rowSums(abs(sgn.m))
+    sigma  <- sqrt(Nr * (Nr + 1) * (2 * Nr + 1)/6)
+    p <- pnorm(W, mean = 0.5, sd = sigma, lower.tail = TRUE)
+    pval <- switch(alternative,
+                    two.sided = pmin(p, 1-p),
+                    greater   = 1-p,
+                    less      = p
+                  )
+    qval <- p.adjust(pval, method = "fdr")
+    sig.cg <- which(qval <= qvalue0)
+  } else {
+    pval <- aaply(dat.m, 1, .fun = function(x){
+                                    wilcox.test(x, alternative = alternative)$p.value
+                                   }, .parallel = TRUE
+                  )
+    qval <- p.adjust(pval, method = "fdr")
+    sig.cg <- which(qval <= qvalue0)
+  }
   
   table <- data.frame(pvalue = pval, qvalue = qval)
   rownames(table) <- rownames(dat.m)
   
   res <- list(siggene = table[sig.cg,], null = table[-sig.cg,])
+  res
 }
