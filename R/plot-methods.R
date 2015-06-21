b14d46a5561cad7e233de13890aed249403d7395
xinchoubiology <xinchoubiology@gmail.com>
2015-06-20 21:52:13 -0400

    indentical

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -23,16 +23,17 @@ plot.corr <- function(x, y = NULL, cutoff = 0.8, ...){
   mat <- data.frame(row = rownames(x$cor), x$cor)
   rownames(mat) <- NULL
   mat.m <- melt(mat)
-  mat.m$value<-cut(mat.m$value,breaks=c(-1.01,-abs(cutoff), 0, abs(cutoff), 1.01),include.lowest=FALSE,label=c("TRUE - ","FALSE - ","FALSE + ","TRUE + "))
-  mat.m$row <- factor(mat.m$row, levels=rev(unique(as.character(mat.m$variable))))
-  ggcor <- ggplot(mat.m, aes(row, variable)) +
-            geom_tile(aes(fill=value),colour="black")  +
-            scale_fill_brewer(palette = "RdYlGn",name="Correlation") + 
-            theme(axis.text.x=element_text(angle=-90), 
-                  axis.title=element_blank()) +
-            theme(panel.background=element_blank(),
-                  panel.grid.minor=element_blank(),
-                  panel.grid.major=element_blank())
+  mat.m$value <- cut(mat.m$value,breaks=c(-1.01,-abs(cutoff), 0, abs(cutoff), 1.01),include.lowest=FALSE,label=c("TRUE - ","FALSE - ","FALSE + ","TRUE + "))
+  mat.m$row   <- factor(mat.m$row, levels=rev(unique(as.character(mat.m$variable))))
+  ggcor       <- ggplot(mat.m, aes(row, variable)) +
+                    geom_tile(aes(fill=value),colour="black")  +
+                    scale_fill_brewer(palette = "RdYlGn",name="Correlation") + 
+                    theme(axis.text.x=element_text(angle=-90), 
+                          axis.title=element_blank()) +
+                    theme(panel.background=element_blank(),
+                          panel.grid.minor=element_blank(),
+                          panel.grid.major=element_blank())
   ggcor
 }
 
+#' 
