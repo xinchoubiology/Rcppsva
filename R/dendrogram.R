0d309b9d5c1aac61ab64ac734d3bc0a3c2eb4f7a
xinchoubiology <xinchoubiology@gmail.com>
2015-06-22 10:07:34 -0400

    correct dendrogram

--- R/dendrogram.R
+++ R/dendrogram.R
@@ -50,25 +50,27 @@ dendro.data.hclust <- function(model, type = c("rectangle", "triangle"), ...){
 #' @export
 dendrogram <- function(x, type = c("rectangle", "triangle"), ...){
   x1 <- 1
-  x2 <- .memberDend(x)
+  x2 <- memberDend(x)
   type <- match.arg(type)
   dendronode <- function(x1, x2, subtree, ddsegments = NULL, ddlabels = NULL){
     inner <- !is.leaf(subtree) && x1 != x2
-    bx <- .memberLimit(x1, x2, subtree)
+    bx <- memberLimit(x1, x2, subtree)
     x.top <- bx$x
     y.top <- attr(subtree, "height")
     if(is.leaf(subtree)){
-      ddlabels <- cbind(ddlabels, data.frame(x = x.top, y = 0, label = attr(subtree, "label")))
+      ddlabels <- rbind(ddlabels, data.frame(x = x.top, y = 0, label = attr(subtree, "label")))
     }else if(inner){
       for(k in 1:length(subtree)){
         child <- subtree[[k]]
         y.bot <- attr(child, "height")
-        x.bot <- bx$limit[k:(k+1)] + .midDend(child)
-        if(type == "rectangle"){
-          ddsegments <- cbind(ddsegments, data.frame(x = x.top, y = y.top, xend = x.bot, yend = y.bot))
+        if(is.null(y.bot))
+          y.bot <- 0
+        x.bot <- bx$limit[k] + midDend(child)
+        if(type == "triangle"){
+          ddsegments <- rbind(ddsegments, data.frame(x = x.top, y = y.top, xend = x.bot, yend = y.bot))
         }else{
-          ddsegments <- cbind(ddsegments, data.frame(x = x.top, y = y.top, xend = x.bot, yend = y.top))
-          ddsegments <- cbind(ddsegments, data.frame(x = x.bot, y = y.top, xend = x.bot, yend = y.bot))
+          ddsegments <- rbind(ddsegments, data.frame(x = x.top, y = y.top, xend = x.bot, yend = y.top))
+          ddsegments <- rbind(ddsegments, data.frame(x = x.bot, y = y.top, xend = x.bot, yend = y.bot))
         }
         res <- dendronode(x1 = bx$limit[k], x2 = bx$limit[k+1], child, ddsegments = ddsegments, ddlabels = ddlabels)
         ddsegments <- res$segments
@@ -80,25 +82,26 @@ dendrogram <- function(x, type = c("rectangle", "triangle"), ...){
   res <- dendronode(x1, x2, x, ddsegments = NULL, ddlabels = NULL)
   names(res$segments) <- c("x", "y", "xend", "yend")
   names(res$labels)   <- c("x", "y", "label")
+  class(res) <- "dendro"
   res
 }
 
 
 #' assign position of fork and two limit of tree to members
-#' @title .memberLimit 
+#' @title memberLimit 
 #' @param x1 leftmost node 
 #' @param x2 rightmost node
 #' @param subtree subtree of dendrogram
 #' @return list
 #'         x
 #'         limit c(tree1, tree2, tree3)
-.memberLimit <- function(x1, x2, subtree){
+memberLimit <- function(x1, x2, subtree){
   if(!is.leaf(subtree) && x1 != x2){
    k <- length(subtree)
    limit <- integer(k)
    xx <- x1
    for(i in 1:k){
-     m <- .memberDend(substree[[i]])
+     m <- memberDend(subtree[[i]])
      xx <- xx + m
      limit[i] <- xx
    }
@@ -107,11 +110,8 @@ dendrogram <- function(x, type = c("rectangle", "triangle"), ...){
    limit <- c(x1, x2) 
   }
   mid <- attr(subtree, "midpoint")
-  if(!is.numeric(mid)){
-    x <- x1 + (if(!is.leaf(subtree) && x1 != x2)
-                 mid
-               else
-                  0)
+  if(is.numeric(mid)){
+    x <- x1 + mid
   }else{
     x <- mean(c(x1, x2))
   }
@@ -120,9 +120,10 @@ dendrogram <- function(x, type = c("rectangle", "triangle"), ...){
 }
 
 #' return member number in subtree x
+#' @title memberDend
 #' @param subtree
 #' @return number in tree object x
-.memberDend <- function(subtree){
+memberDend <- function(subtree){
   r <- attr(subtree, "members")
   if(is.null(r))
     r <- 1L
@@ -130,9 +131,10 @@ dendrogram <- function(x, type = c("rectangle", "triangle"), ...){
 }
 
 #' return midpoint of subtree
+#' @title midDend
 #' @param subtree
 #' @return midpoint value
-.midDend <- function(subtree){
+midDend <- function(subtree){
   mp <- attr(subtree, "midpoint")
   if(is.null(mp)){
     mp <- 0
--- R/plot-methods.R
+++ R/plot-methods.R
@@ -36,4 +36,46 @@ plot.corr <- function(x, y = NULL, cutoff = 0.8, ...){
   ggcor
 }
 
-#' 
+#' plot hierachical tree for clustering data by ggplot
+#' @rdname plot
+#' @param x dendro class object
+#' @param ... ignored argument
+#' @param rotate if TRUE, rotate tree by 90 degree; Default FALSE
+#' @param labels if TRUE, show segment labels
+#' @method plot dendro
+#' @export
+plot.dendro <- function(x, ..., rotate = FALSE, labels = TRUE){
+  angle <- ifelse(rotate, 0, 90)
+  hjust <- ifelse(rotate, 0, 1)
+  p <- ggplot() + 
+       geom_segment(data = x$segments, aes_string(x = "x", y = "y", xend = "xend", yend = "yend"))
+  if(labels){
+    p <- p + scale_x_discrete(labels = x$labels$label)
+  }
+  if(rotate){
+    p <- p + coord_flip()
+    p <- p + scale_y_continuous()
+  }else{
+    p <- p + scale_y_continuous()
+  }
+  p <- p + theme_dendro()
+  p <- p + theme(axis.text.x = element_text(angle = angle, hjust = 1))
+  p <- p + theme(axis.text.y = element_text(angle = angle, hjust = 1))
+  p
+}
+
+
+#' create complete blank theme in ggplot for dendro class object
+#' @title theme_dendro
+#' @export
+theme_dendro <- function(){
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.title.x     = element_text(colour = NA),
+        axis.title.y     = element_blank(),
+        axis.text.x      = element_blank(),
+        axis.text.y      = element_blank(),
+        axis.line        = element_blank(),
+        axis.ticks       = element_blank())
+}
